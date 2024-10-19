d7635e0ae77c2f07bca4692acd66f36cce23bb89
CodeAmboka <ambokadzedato0@gmail.com>
2024-10-19 16:58:29 +0400

    clerk auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { Modal } from "@/components/ui/modal"
+import { UserButton } from "@clerk/nextjs"
+
+const SetupPage = () => {
+  return (
+    <div>
+      <Modal title="test" description="test" isOpen onClose={() => {}}>
+        children
+      </Modal>
+    </div>
+  )
+}
+export default SetupPage
