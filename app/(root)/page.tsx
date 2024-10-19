7aacf3ac9c97f5c2a17a970caa9a9753e311d07a
ItsYoungCoder <ambokadzedato0@gmail.com>
2024-10-19 17:31:43 +0400

    change main page test title

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,7 +6,7 @@ const SetupPage = () => {
   return (
     <div>
       <Modal title="test" description="test" isOpen onClose={() => {}}>
-        Hello, I am David Ambokadze
+        Hello, I am David Ambokadze from the Georgia
       </Modal>
     </div>
   )
